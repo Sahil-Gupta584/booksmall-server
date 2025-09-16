0f55978fb6fdebbf6e10d02d572b5a9927908111
Sahil Gupta <guptas3067@gmail.com>
2025-09-16 18:00:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,65 +36,7 @@ const broadcastOnlineUsers = () => {
 };
 
 
-io.on("connection", (socket) => {
-    console.log("Socket Connection done");
 
-    socket.on('REGISTER_USER', (userId) => {
-
-        onlineUsers.set(userId, socket.id);
-        broadcastOnlineUsers()
-        console.log('OnlineUsers', onlineUsers);
-
-        // Deliver any offline messages for this user
-        const offlineMessageQueue = offlineMessages.get(userId) || [];
-        console.log('offlinemessages', offlineMessageQueue)
-        offlineMessageQueue.forEach(message => {
-            console.log('from offline message')
-            io.to(socket.id).emit('RECEIVE_MSG_EVENT', message);
-        });
-        offlineMessages.delete(userId);
-    });
-
-    socket.on("TYPING_EVENT", ({ partnerId, isTyping }) => {
-
-        const partnerSocketId = onlineUsers.get(partnerId);
-        console.log('partnerSocketId', partnerSocketId)
-        io.to(partnerSocketId).emit("TYPING_EVENT", { partnerId, isTyping })
-
-    })
-
-    socket.on("SEEN_MESSAGE", ({ chatId, partnerId }) => {
-        const partnerSocketId = onlineUsers.get(partnerId);
-        console.log('partnerSocketId', partnerSocketId);
-        console.log('emitting seen message from backend');
-        io.to(partnerSocketId).emit("SEEN_MESSAGE", { chatId })
-
-    })
-
-    socket.on('SEND_MESSAGE', (newMessage) => {
-        const receiverSocketId = onlineUsers.get(newMessage.receiverId);
-        if (receiverSocketId) {
-            console.log('sendin for online');
-            io.to(receiverSocketId).emit('RECEIVE_MSG_EVENT', newMessage);
-        } else {
-            // Store the message for later delivery
-            console.log('saving for offline')
-            let offlineMessageQueue = offlineMessages.get(newMessage.receiverId) || [];
-            offlineMessageQueue.push(newMessage);
-            offlineMessages.set(newMessage.receiverId, offlineMessageQueue);
-        }
-    });
-
-    socket.on('disconnect', () => {
-        for (const [userId, socketId] of onlineUsers.entries()) {
-            if (socketId === socket.id) {
-                onlineUsers.delete(userId);
-                break;
-            }
-        }
-        broadcastOnlineUsers();
-    });
-});
 
 
 const PORT = process.env.PORT || 3001;
